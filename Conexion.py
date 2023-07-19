d29a105d850e10886857f15c7081baca3f05cabf
TinchoARS <fradejas84@gmail.com>
2023-07-19 09:29:55 -0300

    conexion

--- Conexion.py
+++ Conexion.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conexion = mysql.connector.connect(user='root', password='Marman6403', host='localhost', database='calendario', port='3306')
+conexion = mysql.connector.connect(user='root', password='martin', host='localhost', database='calendario', port='3306')
 
 print(conexion)
 
